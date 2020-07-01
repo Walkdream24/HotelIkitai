fbba780e690836ee5a7d94deafb2a223420cdadf
Walkdream24 <42060447+Walkdream24@users.noreply.github.com>
2020-07-01 13:35:37 +0900

    Initial Commit

---
+++ HotelIkitai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelIkitai
+//
+//  Created by 中重歩夢 on 2020/07/01.
+//  Copyright © 2020 Ayumu Nakashige. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
